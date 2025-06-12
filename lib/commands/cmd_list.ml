66ecb3ad9b8e70b0f91a611004e6d39a9a5bc524
Adam Buchweitz <adam@buchweitz.life>
2025-06-12 16:32:52 -0500

    feat: add support for sorting books
    Traditional, alphabetical, chronological

--- lib/commands/cmd_list.ml
+++ lib/commands/cmd_list.ml
@@ -1,9 +1,14 @@
-open Cmdliner
-open Cmdliner.Term.Syntax
 open Printf
 open Models
 
-type list_type = Translations | Books
+let sort_by_traditional_order books = 
+  List.sort (fun a b -> compare a.traditional_order b.traditional_order) books
+
+let sort_by_alphabetical_order books =
+  List.sort (fun a b -> compare a.alphabetical_order b.alphabetical_order) books
+
+let sort_by_chronological_order books =
+  List.sort (fun a b -> compare a.chronological_order b.chronological_order) books
 
 let list_translations () =
   let translations_list = Api.fetch_translations in
@@ -11,14 +16,50 @@ let list_translations () =
   |> List.fold_left (fun acc item -> sprintf "%s\n%10s | %s" acc item.shortName item.englishName) ""
   |> print_endline
 
-let list_books ?(translation = "BSB") () =
-  let book_list = Api.fetch_books translation in
-  List.fold_left (fun acc item -> sprintf "%s\n%02d - %s" acc item.order item.name ) "" book_list
+type sort_order = Traditional | Alphabetical | Chronological
+let sort_str = function
+  | Traditional -> "traditional" | Alphabetical -> "alphabetical" | Chronological -> "chronological"
+
+let list_books sort_order =
+  let sort = match sort_order with
+    | Traditional -> sort_by_traditional_order
+    | Alphabetical -> sort_by_alphabetical_order
+    | Chronological -> sort_by_chronological_order
+  in
+  bible_books
+  |> sort
+  |> List.fold_left (fun acc item -> sprintf "%s\n%02d | %s" acc item.traditional_order item.name) ""
   |> print_endline
 
 (*Constants*)
 let s_translations = "translations"
 let s_books = "books"
+
+type list_type = Translations | Books
+
+let list target ~sort_order () =
+  Printf.printf "The books of the Bible, sorted %sly:\n" (sort_str sort_order);
+  match target with
+  | Books -> list_books sort_order
+  | Translations -> list_translations ()
+
+open Cmdliner
+open Cmdliner.Term.Syntax
+
+let sort_order=
+  let traditional =
+    let doc = "Sort traditionally." in
+    Traditional, Arg.info ["t"; "traditional"] ~doc
+  in
+  let alphabetical =
+    let doc = "Sort alphabetically." in
+    Alphabetical, Arg.info ["a"; "alphabetical"] ~doc
+  in
+  let chronological =
+    let doc = "Sort chronologically." in
+    Chronological, Arg.info ["c"; "chronological"] ~doc
+  in
+  Arg.(last & vflag_all [Traditional] [traditional; alphabetical; chronological])
 
 let list_type_conv =
   let parse = function
@@ -30,15 +71,11 @@ let list_type_conv =
     | Books -> Format.fprintf fmt "%s" s_books in
   Arg.conv (parse, print)
 
-let list ?(translation = "BSB") = function
-  | Books -> list_books ~translation ()
-  | Translations -> list_translations ()
-
 let arg_type = 
   let doc = sprintf "Valid arguments are: %s, %s" s_translations s_books in
   Arg.(required & pos 0 (some list_type_conv) None & info [] ~doc ~docv: "TYPE" )
 
 let cmd =
   Cmd.v (Cmd.info "list" ~doc:"List biblical contents") @@
-  let+ arg_type in
-  list arg_type
+  let+ arg_type and+ sort_order in
+  list arg_type ~sort_order ()
